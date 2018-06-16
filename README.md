06730a886c54a6fd3d8f69e6202772d5c5fcce8d
Rodrigo Souza <rsouza01@gmail.com>
2018-06-16 20:39:41 -0300

    First version

---
+++ README.md
@@ -0,0 +1,20 @@
+# Rest micro-service
+
+My study files on how to implement a micro-serivce on C++ using the C++ REST SDK.
+
+
+# How to build
+	$ make
+
+# How to run
+	$ ./bin/restws
+
+
+# References
+
+https://github.com/Microsoft/cpprestsdk/wiki/Getting-Started-Tutorial
+https://github.com/Microsoft/cpprestsdk/wiki
+https://github.com/Microsoft/cpprestsdk/wiki/Samples
+https://hiltmon.com/blog/2013/07/03/a-simple-c-plus-plus-project-structure/
+https://github.com/ivanmejiarocha/micro-service
+https://medium.com/audelabs/modern-c-micro-service-implementation-rest-api-b499ffeaf898
---
+++ src/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+#include <cpprest/http_client.h>
+#include <cpprest/filestream.h>
+
+#include <cpprest/http_listener.h>              // HTTP server
+#include <cpprest/json.h>                       // JSON library
+#include <cpprest/uri.h>                        // URI library
+#include <cpprest/ws_client.h>                  // WebSocket client
+#include <cpprest/containerstream.h>            // Async streams backed by STL containers
+#include <cpprest/interopstream.h>              // Bridges for integrating Async streams with STL and WinRT streams
+#include <cpprest/rawptrstream.h>               // Async streams backed by raw pointer to memory
+#include <cpprest/producerconsumerstream.h>     // Async streams for producer consumer scenarios
+
+using namespace utility;                    // Common utilities like string conversions
+using namespace web;                        // Common features like URIs.
+using namespace web::http;                  // Common HTTP functionality
+using namespace web::http::client;          // HTTP client features
+using namespace concurrency::streams;       // Asynchronous streams
+
+using namespace std;
+
+int foo [] = {16, 2, 77, 40, 12071};
+int n, result=0;
+
+int main ()
+{
+  for (n = 0 ; n < 5 ; ++n )
+  {
+    result += foo[n];
+  }
+  
+  cout << result << endl;
+  
+  return 0;
+}
